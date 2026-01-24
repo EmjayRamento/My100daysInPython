b82235a7fc105898eaa15e50bf6e6c2b9da93524
EmjayRamento <ramento.emjay.cs@gmail.com>
2026-01-24 22:42:38 +0800

    Add files via upload
    I did an variable and inputing practice and also string manipulation. Looking forward for more complex lessons :)

---
+++ try.py
@@ -0,0 +1,43 @@
+# # message = 5
+
+# # if message == 5:
+# #     print("hahahahaha")
+# # else:
+# #     print("sdfsdfsf")
+
+# # message = 5
+
+# # print("ako"); print ("si"); print("emjay")?]
+
+# # print('ako ', end='')
+# # print('hahahahaa')
+
+# # print(12344342354345)
+# # print(3*2+4)
+
+# # x = 5
+# # y = 'emjay'
+
+# # print(type(x))
+# # print(type(y))
+
+# # print ('hello'*5)
+
+# # day = input('what day is today?')
+
+# # if day == 'wed':
+# #     print('ulol')
+# # else:
+# #     print('lols')
+
+# # #Exercises
+# # name = input('yourname:')
+# # color = input('colors:')
+
+# # print('Hi, My name is '+ name + ' my color is '+ color)
+
+# a = input('a= ')
+# b = input ('b=')
+
+# print('a =', a)
+# print('b = ', b)
